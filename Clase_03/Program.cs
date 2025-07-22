8cf8196757c20392082f65e9f401f68f3ca4a3ca
yony-Dev <benitezalexander661@gmail.com>
2025-07-22 14:19:48 -0600

    Agregar archivos de proyecto.

---
+++ Clase_03/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Una empresa constructora necesita calcular el área de un terreno rectangular.
+            //Pide al usuario que ingrese la base y la altura en metros, y muestra el área
+            //total.
+
+            //    Console.Write("Ingrese la base en metros:");
+            //    double baseReact = double.Parse(Console.ReadLine());
+
+            //    Console.Write("Ingrese la altura en metros:");
+            //    double altura = double.Parse(Console.ReadLine());
+
+            //    double area = baseReact * altura;
+            //    Console.WriteLine($"El area del terreno es: {area}");
+
+
+
+            /* Un docente desea calcular el promedio de tres notas de un estudiante. Solicita
+            las tres calificaciones y muestra el promedio final.
+
+            Console.Write("Ingrese la primera calificacion:");
+            double nota1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese la segunda calificacion:");
+            double nota2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese la tercera calificacion:");
+            double nota3 = double.Parse(Console.ReadLine());
+
+            double promedio = (nota1 + nota2 + nota3 ) / 3;
+
+            Console.WriteLine($"El promedio final es: {promedio}");*/
+
+
+            /*Una aplicación meteorológica debe convertir una temperatura de grados
+            Celsius a Fahrenheit. Pide al usuario que ingrese la temperatura en Celsius y
+            muestra el resultado en Fahrenheit.
+
+            Console.Write("Ingrese la temperatura en °C:");
+            double gradosCelsius = double.Parse(Console.ReadLine());
+
+            double gradosFarenheit = (gradosCelsius * 9 / 5) + 32;
+            Console.WriteLine($"{gradosCelsius}°C a farenheit = {gradosFarenheit}°F");*/
+
+
+            /*Un turista desea saber cuántos colones recibirá por una cantidad en dólares.
+            La tasa de cambio actual es de 8.75. Solicita al usuario la cantidad en dólares
+            y muestra el equivalente en colones.
+
+            Console.Write("Ingrese la cantidad en dolares:");
+            double dolares = double.Parse(Console.ReadLine());
+
+            double colones = dolares * 8.75;
+            Console.WriteLine($"{dolares}$ dolares son {colones} colones");*/
+
+
+            /*Una aplicación de salud necesita calcular cuántos meses y días ha vivido una
+            persona. Pide al usuario que ingrese su edad en años y muestra la cantidad
+            aproximada de meses y días vividos
+            Console.Write("Ingrese su edad en años:");
+            int edad = int .Parse(Console.ReadLine());
+
+            int meses = edad * 12;
+            int dias = edad * 365;
+
+            Console.WriteLine($"Su edad es aproximadamnete {meses} meses y {dias} dias");*/
+
+
+            /*Un ingeniero debe calcular el perímetro de un terreno con forma de triángulo.
+            Solicita al usuario que ingrese las medidas de los tres lados y muestra el
+            perímetro total.
+            Console.Write("Ingrese la primer medida:");
+            double medida1 = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese la segunda medida:");
+            double medida2 = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese la tercera medida:");
+            double medida3 = double.Parse(Console.ReadLine());
+
+            double perimetro = medida1 + medida2 + medida3;
+            Console.WriteLine($"El perimetro es: {perimetro}");*/
+
+            /*Un programa educativo requiere mostrar el cuadrado y la raíz cuadrada de un
+            número ingresado por el usuario. Solicita el número y muestra ambos
+            resultados.
+            Console.Write("Ingrese el numero:");
+            double numero = double.Parse(Console.ReadLine());
+
+            double cuadrado = Math.Pow(numero, 2);
+            double raiz = Math.Sqrt(numero);
+
+            Console.WriteLine($"Cuadrado: {cuadrado}");
+            Console.WriteLine($"Raiz Cuadrada: {raiz}");*/
+
+        }
+    }
+}
