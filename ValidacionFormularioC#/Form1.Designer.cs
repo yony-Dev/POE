89ecf20f759ad8342e94fdcfdc548a669cebb18c
Yony Alexander Benitez Salazar <benitezalexander661@gmail.com>
2025-10-30 10:16:47 -0600

    TareaValidacionFormularioC#

---
+++ ValidacionFormularioC#/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace FormularioPOE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnValidar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lalMostrarInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.BackColor = System.Drawing.SystemColors.Info;
+            this.txtEdad.Location = new System.Drawing.Point(264, 185);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(60, 22);
+            this.txtEdad.TabIndex = 0;
+            this.txtEdad.TextChanged += new System.EventHandler(this.txtEdad_TextChanged);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.BackColor = System.Drawing.SystemColors.Info;
+            this.txtNombre.Location = new System.Drawing.Point(264, 90);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(417, 22);
+            this.txtNombre.TabIndex = 1;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
+            // 
+            // btnValidar
+            // 
+            this.btnValidar.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnValidar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnValidar.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.btnValidar.Location = new System.Drawing.Point(398, 175);
+            this.btnValidar.Name = "btnValidar";
+            this.btnValidar.Size = new System.Drawing.Size(110, 32);
+            this.btnValidar.TabIndex = 2;
+            this.btnValidar.Text = "Validar";
+            this.btnValidar.UseVisualStyleBackColor = false;
+            this.btnValidar.Click += new System.EventHandler(this.btnValidar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Info;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(53, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ingrese su nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Info;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(53, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ingrese su edad";
+            // 
+            // lalMostrarInfo
+            // 
+            this.lalMostrarInfo.AutoSize = true;
+            this.lalMostrarInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lalMostrarInfo.Location = new System.Drawing.Point(53, 272);
+            this.lalMostrarInfo.Name = "lalMostrarInfo";
+            this.lalMostrarInfo.Size = new System.Drawing.Size(17, 25);
+            this.lalMostrarInfo.TabIndex = 5;
+            this.lalMostrarInfo.Text = ".";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lalMostrarInfo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnValidar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtEdad);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnValidar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lalMostrarInfo;
+    }
+}
+
---
+++ ValidacionFormularioC#/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormularioPOE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+            if (!Regex.IsMatch(txtNombre.Text, @"^[a-zA-Z\s]*$" ))
+            {
+                MessageBox.Show("Por favor, ingrese solo letras y espacios en el nombre.", "Entrada inválida", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtNombre.Text = string.Empty;
+            }
+        }
+
+        private void txtEdad_TextChanged(object sender, EventArgs e)
+        {
+            if (!Regex.IsMatch(txtEdad.Text, @"^\d*$"))
+            {
+                MessageBox.Show("Por favor, ingrese solo números en la edad.", "Entrada inválida", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtEdad.Text = string.Empty;
+            }
+        }
+
+        private void btnValidar_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text.Trim();
+            string edadTexto = txtEdad.Text.Trim();
+            if (string.IsNullOrEmpty(nombre) || string.IsNullOrEmpty(edadTexto))
+            {
+                MessageBox.Show("Por favor, complete todos los campos.", "Campos incompletos", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            if (int.TryParse(edadTexto, out int edadNum))
+            {
+                if (edadNum >= 18 & edadNum <= 50)
+                {
+                    MessageBox.Show($"Bienvenido, {nombre}. Usted es mayor de edad.", "Acceso permitido", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show($"Acceso Denegado. {nombre} Usted es menor de edad.", "Acceso denegado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingrese una edad válida.", "Entrada inválida", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
